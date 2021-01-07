07c1c7295fe6464f93c324f1c2d1497cd78d19fe
Flukeznpm <nattapong.meun@gmail.com>
2021-01-08 02:06:35 +0700

    + blog

---
+++ be/routes/blog.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/', function (req, res, next) {
+    res.render("blog");
+});
+
+router.get('/add', function (req, res, next) {
+    res.render("addBlog");
+});
+
+module.exports = router
--- be/server.js
+++ be/server.js
@@ -4,7 +4,6 @@ const path = require('path') // เรียกใช้งาน path module
 const createError = require('http-errors') // เรียกใช้งาน http-errors module
 const port = 3000  // port 
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set("view engine", "ejs");
 
@@ -12,26 +11,13 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
 
-// ส่วนของการใช้งาน router module ต่างๆ 
 const indexRouter = require('./routes/index')
 const userRouter = require('./routes/users')
+const blogRouter = require('./routes/blog')
+
 app.use('/', indexRouter)
 app.use('/user', userRouter)
-
-// ทำงานทุก request ที่เข้ามา 
-app.use(function (req, res, next) {
-    var err = createError(404)
-    next(err)
-})
-
-// ส่วนจัดการ error
-app.use(function (err, req, res, next) {
-    // กำหนด response local variables 
-    res.locals.message = err.message
-    res.locals.error = req.app.get('env') === 'development' ? err : {}
-    res.status(err.status || 500)
-    res.render('error')
-})
+app.use('/blog', blogRouter)
 
 app.listen(port, () => {
     console.log(`Server is running on port : ${port}`)
